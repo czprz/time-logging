7daf1cad85c72fbcec6f4aec3bf28c6ac1098b82
Casper Overholm Elkrog <support.open-source@elkrog.me>
2023-05-07 22:39:29 +0200

    Added endpoint for getting tracking hours

---
+++ TimeLogging/Endpoints/Tracking/GetTracking.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning.Builder;
+
+namespace TimeLogging.Endpoints.Tracking;
+
+public static class GetTracking
+{
+    public static void MapTrackingLatestEndpoints(this WebApplication app, ApiVersionSet apiVersionSet)
+    {
+        app.MapGet("tracking", GetLatest)
+            .WithApiVersionSet(apiVersionSet)
+            .MapToApiVersion(1)
+            .CacheOutput("expires5s");
+    }
+
+    private static async Task<IResult> GetLatest(HttpContext context)
+    {
+        return Results.Ok();
+    }
+}
